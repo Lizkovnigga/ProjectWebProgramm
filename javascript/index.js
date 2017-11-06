c03283a02d626f012080a6e634d6a917ec36dcd0
Lizkovnigga <TwitterLK@mail.ru>
2017-11-06 21:32:42 +0300

    commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,22 @@
+var  link= document.querySelector(".aboutcompany a");
+
+link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    var darkLayer = document.createElement('div');
+    darkLayer.id = 'shadow';
+    document.body.appendChild(darkLayer);
+
+    var modalWin2 = document.getElementById('popupWin2');
+    modalWin2.style.display = 'block';
+
+    darkLayer.onclick = function () {
+        darkLayer.parentNode.removeChild(darkLayer);
+        modalWin2.style.display = 'none'; // делаем окно невидимым
+    };
+
+});
+
 function showModalWin() {
 
     var darkLayer = document.createElement('div');
