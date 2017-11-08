3048853ba453f0357f5ba17f611da34d8fcb34ed
Lizkovnigga <TwitterLK@mail.ru>
2017-11-08 21:40:09 +0300

    commit

--- javascript/index.js
+++ javascript/index.js
@@ -1,28 +1,47 @@
 var  link= document.querySelector(".aboutcompany a");
 
-
 link.addEventListener("click", function (e) {
     e.preventDefault();
-
-    var darkLayer = document.createElement('div');
-    darkLayer.id = 'shadow';
-    document.body.appendChild(darkLayer);
-
     var modalWin2 = document.getElementById('popupWin2');
     modalWin2.style.display = 'block';
 
-    darkLayer.onclick = function () {
-        darkLayer.parentNode.removeChild(darkLayer);
-        modalWin2.style.display = 'none'; // делаем окно невидимым
-    };
-
 });
-
 
 var  link2= document.querySelector(".servicesandmap a img");
 
 link2.addEventListener("click", function (y) {
     y.preventDefault();
+
+    var modalWin3 = document.getElementById('popupWin3');
+    modalWin3.style.display = 'block';
+
+});
+
+var  link3= document.querySelector(".closemodalwin2");
+
+link3.addEventListener("click", function (q) {
+    q.preventDefault();
+});
+
+function hideA()
+{
+    a = document.getElementById("popupWin");
+    b=document.getElementById("popupWin2");
+    a.style.display = "none";
+    b.style.display = "none";
+};
+
+function showModalWin() {
+
+    var modalWin = document.getElementById('popupWin');
+    modalWin.style.display = 'block';
+
+};
+
+var  link4= document.querySelector(".servicesandmap a img");
+
+link4.addEventListener("click", function (s) {
+    s.preventDefault();
 
     var darkLayer = document.createElement('div');
     darkLayer.id = 'shadow';
@@ -38,17 +57,4 @@ link2.addEventListener("click", function (y) {
 
 });
 
-function showModalWin() {
 
-    var darkLayer = document.createElement('div');
-    darkLayer.id = 'shadow';
-    document.body.appendChild(darkLayer);
-
-    var modalWin = document.getElementById('popupWin');
-    modalWin.style.display = 'block';
-
-    darkLayer.onclick = function () {
-        darkLayer.parentNode.removeChild(darkLayer);
-        modalWin.style.display = 'none'; // делаем окно невидимым
-    };
-}
